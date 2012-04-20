a66ccc6b94da358e60f61a20c1c5e9108318f85f
bobbylight <robert@fifesoft.com>
2012-04-20 13:48:26 +0000

    License change to modified BSD.

--- src/org/fife/ui/AboutDialog.java
+++ src/org/fife/ui/AboutDialog.java
@@ -3,22 +3,9 @@
  *
  * AboutDialog.java - An About dialog for an application.
  * Copyright (C) 2003 Robert Futrell
- * robert_futrell at users.sourceforge.net
- * http://fifesoft.com
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * http://fifesoft.com/rtext
+ * Licensed under a modified BSD license.
+ * See the included license file for details.
  */
 package org.fife.ui;
 
@@ -254,7 +241,7 @@ public class AboutDialog extends EscapableDialog implements ActionListener {
 
 
 	/**
-	 * A dialog box that displays the GNU GPL.
+	 * A dialog box that displays the license for the application.
 	 */
 	protected class LicenseDialog extends EscapableDialog
 			implements ActionListener {
